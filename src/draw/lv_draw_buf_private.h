7cb67c3c7e4aec1ef32ddf714a17ee3152283ddd
zwang <wangzhengwei1991@hotmail.com>
2025-01-08 11:36:00 +0800

    use GPU for memcopy and memclear

--- src/draw/lv_draw_buf_private.h
+++ src/draw/lv_draw_buf_private.h
@@ -31,6 +31,8 @@ struct _lv_draw_buf_handlers_t {
     lv_draw_buf_cache_operation_cb invalidate_cache_cb;
     lv_draw_buf_cache_operation_cb flush_cache_cb;
     lv_draw_buf_width_to_stride_cb width_to_stride_cb;
+    lv_draw_buf_clear_cb clear_cb;
+    lv_draw_buf_copy_cb copy_cb;
 };
 
 /**********************
