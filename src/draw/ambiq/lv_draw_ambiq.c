925470ddbff5f445f6bca3905305ca9d0f2cf5c8
Zhengwei Wang <zwang@ambiq.com>
2025-01-08 11:38:36 +0800

    1, added mutex to lock the GPU context, enable call NemaSDK APIs frrm multiple thread

--- src/draw/ambiq/lv_draw_ambiq.c
+++ src/draw/ambiq/lv_draw_ambiq.c
@@ -41,6 +41,8 @@ static int32_t lv_draw_ambiq_delete(lv_draw_unit_t * draw_unit);
  *  STATIC VARIABLES
  **********************/
 #define _draw_info LV_GLOBAL_DEFAULT()->draw_info
+static lv_draw_ambiq_unit_t * draw_ambiq_unit = NULL;
+
 
 /**********************
  *      MACROS
@@ -52,7 +54,9 @@ static int32_t lv_draw_ambiq_delete(lv_draw_unit_t * draw_unit);
 
 void lv_draw_ambiq_init(void)
 {
-    lv_draw_ambiq_unit_t * draw_ambiq_unit = lv_draw_create_unit(sizeof(lv_draw_ambiq_unit_t));
+    lv_draw_ambiq_init_buf_handlers();
+
+    draw_ambiq_unit = lv_draw_create_unit(sizeof(lv_draw_ambiq_unit_t));
     draw_ambiq_unit->base_unit.dispatch_cb = dispatch;
     draw_ambiq_unit->base_unit.evaluate_cb = evaluate;
     draw_ambiq_unit->base_unit.delete_cb = LV_USE_OS ? lv_draw_ambiq_delete : NULL;
@@ -60,14 +64,19 @@ void lv_draw_ambiq_init(void)
     draw_ambiq_unit->total_buffer = 0;
     draw_ambiq_unit->total_cl = 0;
     draw_ambiq_unit->small_texture_buffer_size_byte = 256;
-    draw_ambiq_unit->small_texture_buffer  = nema_buffer_create_pool(NEMA_MEM_POOL_CL_RB, draw_ambiq_unit->small_texture_buffer_size_byte);
+    draw_ambiq_unit->small_texture_buffer  = nema_buffer_create_pool(NEMA_MEM_POOL_FB, draw_ambiq_unit->small_texture_buffer_size_byte);
 #if LV_USE_VECTOR_GRAPHIC
     draw_ambiq_unit->vg_path = nema_vg_path_create();
     draw_ambiq_unit->vg_paint = nema_vg_paint_create();
     draw_ambiq_unit->vg_grad = nema_vg_grad_create();
 #endif
 
+//    lv_ll_init(&draw_ambiq_unit->inserted_cl_ll, sizeof(nema_cmdlist_t));
+
+    draw_ambiq_unit->nema_context_lock_count = 0;
+
 #if LV_USE_OS
+    lv_mutex_init(&draw_ambiq_unit->mutex_nema_context);
     lv_thread_init(&draw_ambiq_unit->thread, LV_THREAD_PRIO_HIGH, render_thread_cb, 8 * 1024, draw_ambiq_unit);
 #endif
 
@@ -134,6 +143,9 @@ static int32_t lv_draw_ambiq_delete(lv_draw_unit_t * draw_unit)
 
     //Release VG gradient
     nema_vg_grad_destroy(draw_ambiq_unit->vg_grad);
+
+    //Release inserted command list
+    // lv_ll_clear(&draw_ambiq_unit->inserted_cl_ll);
 
 #if LV_USE_OS
     LV_LOG_INFO("cancel Ambiq GPU rendering thread");
@@ -291,10 +303,7 @@ static void render_thread_cb(void * ptr)
     u->inited = true;
 
     while(1) {
-        while(u->task_act == NULL) {
-            if(u->exit_status) {
-                break;
-            }
+        while(u->task_act == NULL && !u->exit_status) {
             lv_thread_sync_wait(&u->sync);
         }
 
@@ -303,11 +312,35 @@ static void render_thread_cb(void * ptr)
             break;
         }
 
+        // if(!lv_ll_is_empty(&u->inserted_cl_ll))
+        // {
+        //     lv_mutex_lock(&u->mutex_inserted_cl_ll);
+        //     nema_cmdlist_t * cl = lv_ll_get_head(&u->inserted_cl_ll);
+        //     while(cl)
+        //     {
+
+        //         lv_ll_remove(&u->inserted_cl_ll, cl);
+        //         lv_mutex_unlock(&u->mutex_inserted_cl_ll);
+
+        //         nema_cl_submit(cl);
+        //         nema_cl_wait(cl);
+        //         nema_cl_destroy(cl);
+        //         lv_free(cl);
+
+
+        //         lv_mutex_lock(&u->mutex_inserted_cl_ll);
+        //         cl = lv_ll_get_head(&u->inserted_cl_ll);
+        //     }
+
+        //     lv_mutex_unlock(&u->mutex_inserted_cl_ll);
+        // }
+
         execute_drawing_unit(u);
     }
 
     u->inited = false;
     lv_thread_sync_delete(&u->sync);
+    lv_mutex_delete(&u->mutex_nema_context);
     LV_LOG_INFO("exit software rendering thread");
 }
 #endif
@@ -337,6 +370,8 @@ static void execute_drawing(lv_draw_ambiq_unit_t * u)
     lv_draw_buf_invalidate_cache(draw_buf, &draw_area);
 
     nema_cmdlist_t cl = nema_cl_create_sized(10*1024);
+
+    lv_draw_ambiq_nema_context_lock();
 
     nema_cl_bind_circular(&cl);
 
@@ -400,6 +435,8 @@ static void execute_drawing(lv_draw_ambiq_unit_t * u)
 
     nema_cl_submit(&cl);
 
+    lv_draw_ambiq_nema_context_unlock();
+
     nema_cl_wait(&cl);
 
     uint32_t nema_error_code = nema_get_error();
@@ -419,5 +456,79 @@ static void execute_drawing(lv_draw_ambiq_unit_t * u)
 
     LV_PROFILER_END;
 }
+
+lv_result_t lv_draw_ambiq_nema_context_lock(void)
+{
+#if LV_USE_OS
+    LV_ASSERT_MSG(draw_ambiq_unit != NULL, "Ambiq GPU draw unit is not initialized!");
+    lv_result_t res = lv_mutex_lock(&draw_ambiq_unit->mutex_nema_context);
+    if(res != LV_RESULT_OK)
+    {
+        LV_LOG_ERROR("Ambiq GPU mutex lock failed!");
+    }
+    else
+    {
+        draw_ambiq_unit->nema_context_lock_count ++;
+        LV_ASSERT_MSG(draw_ambiq_unit->nema_context_lock_count == 1, "Ambiq GPU mutex lock count error!");
+    }
+    return res;
+#else
+    draw_ambiq_unit->nema_context_lock_count ++;
+    LV_ASSERT_MSG(draw_ambiq_unit->nema_context_lock_count == 1, "Ambiq GPU mutex lock count error!");
+    return LV_RESULT_OK;
+#endif
+}
+
+lv_result_t lv_draw_ambiq_nema_context_unlock(void)
+{
+#if LV_USE_OS
+    LV_ASSERT_MSG(draw_ambiq_unit != NULL, "Ambiq GPU draw unit is not initialized!");
+    lv_result_t res = lv_mutex_unlock(&draw_ambiq_unit->mutex_nema_context);
+    if(res != LV_RESULT_OK)
+    {
+        LV_LOG_ERROR("Ambiq GPU mutex unlock failed!");
+    }
+    else
+    {
+        draw_ambiq_unit->nema_context_lock_count --;
+        LV_ASSERT_MSG(draw_ambiq_unit->nema_context_lock_count == 0, "Ambiq GPU mutex lock count error!");
+    }
+    return res;
+#else
+    draw_ambiq_unit->nema_context_lock_count --;
+    LV_ASSERT_MSG(draw_ambiq_unit->nema_context_lock_count == 0, "Ambiq GPU mutex lock count error!");    
+    return LV_RESULT_OK;
+#endif
+}
+
+// lv_result_t lv_draw_ambiq_nema_buffer_lock(void)
+// {
+// #if LV_USE_OS
+//     LV_ASSERT_MSG(draw_ambiq_unit != NULL, "Ambiq GPU draw unit is not initialized!");
+//     lv_result_t res = lv_mutex_lock(&draw_ambiq_unit->mutex_buffer_create);
+//     if(res != LV_RESULT_OK)
+//     {
+//         LV_LOG_ERROR("Ambiq GPU mutex lock failed!");
+//     }
+//     return res;
+// #else
+//     return LV_RESULT_OK;
+// #endif
+// }
+
+// lv_result_t lv_draw_ambiq_nema_buffer_unlock(void)
+// {
+// #if LV_USE_OS
+//     LV_ASSERT_MSG(draw_ambiq_unit != NULL, "Ambiq GPU draw unit is not initialized!");
+//     lv_result_t res = lv_mutex_unlock(&draw_ambiq_unit->mutex_buffer_create);
+//     if(res != LV_RESULT_OK)
+//     {
+//         LV_LOG_ERROR("Ambiq GPU mutex unlock failed!");
+//     }
+//     return res;
+// #else
+//     return LV_RESULT_OK;
+// #endif
+// }
 
 #endif /*LV_USE_DRAW_AMBIQ*/
