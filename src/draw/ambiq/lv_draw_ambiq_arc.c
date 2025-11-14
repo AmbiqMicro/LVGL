dcb6f214995047aaa78d8368ba2311c2f4b7a4ed
zhichaofeng <zhichao.feng@ambiq.com>
2025-11-14 15:23:53 +0800

    feat: Update the ARGB target to uniformly use the SRC_OVER blending mode.

--- src/draw/ambiq/lv_draw_ambiq_arc.c
+++ src/draw/ambiq/lv_draw_ambiq_arc.c
@@ -111,7 +111,7 @@ void lv_draw_ambiq_arc(lv_draw_task_t * t, const lv_draw_arc_dsc_t * dsc, const 
     uint32_t blending_mode;
 
     if(layer->color_format == LV_COLOR_FORMAT_ARGB8888) {
-        blending_mode = NEMA_BL_SRC_OVER | NEMA_BLOP_SRC_PREMULT;
+        blending_mode = NEMA_BL_SRC_OVER;
     }
     else {
         blending_mode = NEMA_BL_SIMPLE;
