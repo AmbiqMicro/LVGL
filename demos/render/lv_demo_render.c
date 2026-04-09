6f788f382f9458cb41032ba954241163be291483
Richard Wheatley <richard.wheatley@ambiq.com>
2026-04-09 12:18:23 -0500

    demoes: render: add buff static init
    This addition prevents the lvgl from crashing
    
    Signed-off-by: Richard Wheatley <richard.wheatley@ambiq.com>

--- demos/render/lv_demo_render.c
+++ demos/render/lv_demo_render.c
@@ -817,10 +817,15 @@ static void blend_mode_cb(lv_obj_t * parent)
     lv_obj_set_style_bg_color(parent, lv_color_hex(0x808080), 0);
 
     LV_DRAW_BUF_DEFINE_STATIC(buf_rgb565, 36, 30, LV_COLOR_FORMAT_RGB565);
+    LV_DRAW_BUF_INIT_STATIC(buf_rgb565);
     LV_DRAW_BUF_DEFINE_STATIC(buf_rgb888, 36, 30, LV_COLOR_FORMAT_RGB888);
+    LV_DRAW_BUF_INIT_STATIC(buf_rgb888);
     LV_DRAW_BUF_DEFINE_STATIC(buf_xrgb8888, 36, 30, LV_COLOR_FORMAT_XRGB8888);
+    LV_DRAW_BUF_INIT_STATIC(buf_xrgb8888);
     LV_DRAW_BUF_DEFINE_STATIC(buf_argb8888, 36, 30, LV_COLOR_FORMAT_ARGB8888);
+    LV_DRAW_BUF_INIT_STATIC(buf_argb8888);
     LV_DRAW_BUF_DEFINE_STATIC(buf_argb8888_premul, 36, 30, LV_COLOR_FORMAT_ARGB8888_PREMULTIPLIED);
+    LV_DRAW_BUF_INIT_STATIC(buf_argb8888_premul);
 
     /*The canvas will stay in the top left corner to show the original image*/
     lv_obj_t * canvas = lv_canvas_create(lv_screen_active());
